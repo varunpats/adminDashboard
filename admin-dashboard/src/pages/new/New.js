94745d08bd3e1ed3525c7b2ec4c477463daea259
varunpats <varunpatil2502@gmail.com>
2023-12-19 10:15:34 +0530

    Made component logic reusable

--- admin-dashboard/src/pages/new/New.js
+++ admin-dashboard/src/pages/new/New.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
 import './New.scss';
 
-export default function New() {
+export default function New({ inputs, title }) {
+    const [file, setFile] = useState("");
+
+    console.log(file);
     return (
         <div className='new'>
             <Sidebar />
             <div className='container'>
                 <Navbar />
                 <div className='top'>
-                    <h1>Add New User</h1>
+                    <h1>{title}</h1>
                 </div>
                 <div className='bottom'>
                     <div className='left'>
-                        <img src='https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg' alt='' />
+                        <img src={file ?
+                            URL.createObjectURL(file) :
+                            'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'} alt='' />
                     </div>
                     <div className='right'>
                         <form>
@@ -23,36 +28,14 @@ export default function New() {
                                 <label htmlFor='fileUpload'>
                                     Image<UploadFileIcon className='icon' />
                                 </label>
-                                <input type='file' id='fileUpload' style={{ display: 'none' }} />
+                                <input type='file' id='fileUpload' onChange={(e) => setFile(e.target.files[0])} style={{ display: 'none' }} />
                             </div>
-                            <div className='formInput'>
-                                <label>Username</label>
-                                <input type='text' placeholder='john_doe' />
-                            </div>
-                            <div className='formInput'>
-                                <label>Name</label>
-                                <input type='text' placeholder='John Doe' />
-                            </div>
-                            <div className='formInput'>
-                                <label>Email</label>
-                                <input type='email' placeholder='john_doe@gmail.com' />
-                            </div>
-                            <div className='formInput'>
-                                <label>Phone</label>
-                                <input type='text' placeholder='+1 234 567 89' />
-                            </div>
-                            <div className='formInput'>
-                                <label>Password</label>
-                                <input type='password' />
-                            </div>
-                            <div className='formInput'>
-                                <label>Address</label>
-                                <input type='text' placeholder='Elton St. 216 NewYork' />
-                            </div>
-                            <div className='formInput'>
-                                <label>Country</label>
-                                <input type='text' placeholder='USA' />
-                            </div>
+                            {inputs.map((input) => {
+                                return <div className='formInput' key={input.id}>
+                                    <label>{input.label}</label>
+                                    <input type={input.type} placeholder={input.placeholder} />
+                                </div>
+                            })}
                             <button>Send</button>
                         </form>
                     </div>
